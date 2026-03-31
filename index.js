41f843095c0c6009037dd62b471a0ab9d635c0d1
shalone-gichanaa <gichanashalone@gmail.com>
2026-03-31 20:16:27 +0300

    Added JavaScript functions

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+function calculateTax(amount) {
+    return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
 
-
+function isPalindrome(word) {
+    let lowerCaseWord = word.toLowerCase();
+    let reversedWord = lowerCaseWord.split('').reverse().join('');
+    return lowerCaseWord === reversedWord;
+}
+s
+function calculateDiscountedPrice(price, discount) {
+    return price - (price * (discount / 100));
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
